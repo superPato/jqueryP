9c12942b2656828dde2e9c0c418b52e2aaaa362a
Cesar <chechaacual@gmail.com>
2018-08-10 17:12:46 -0600

    Haciendo uso del contexto del controlador de eventos

--- ch03-Handling-Events/03.js
+++ ch03-Handling-Events/03.js
@@ -1,18 +1,26 @@
 $(document).ready(function () {
 
-    $('#switcher-default').on('click', function () {
-        $('body').removeClass('narrow');
-        $('body').removeClass('large');
-    });
+    $('#switcher-default')
+        .addClass('selected')
+        .on('click', function () {
+            $('body').removeClass('narrow');
+            $('body').removeClass('large');
+            $('#switcher button').removeClass('selected');
+            $(this).addClass('selected');
+        });
 
     $('#switcher-narrow').on('click', function () {
         $('body').addClass('narrow');
         $('body').removeClass('large');
+        $('#switcher button').removeClass('selected');
+        $(this).addClass('selected');
     })
 
     $('#switcher-large').on('click', function () {
         $('body').removeClass('narrow');
         $('body').addClass('large');
+        $('#switcher button').removeClass('selected');
+        $(this).addClass('selected');
     });
 
 });
