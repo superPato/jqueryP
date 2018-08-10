c42f78033e49b129d3733687efeccd750e2504cb
Cesar <chechaacual@gmail.com>
2018-08-10 16:27:14 -0600

    Agregar funcionalidad a los botones

--- ch03-Handling-Events/03.js
+++ ch03-Handling-Events/03.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
 
-    
+    $('#switcher-default').on('click', function () {
+        $('body').removeClass('narrow');
+        $('body').removeClass('large');
+    });
+
+    $('#switcher-narrow').on('click', function () {
+        $('body').addClass('narrow');
+        $('body').removeClass('large');
+    })
+
+    $('#switcher-large').on('click', function () {
+        $('body').removeClass('narrow');
+        $('body').addClass('large');
+    });
 
 });
