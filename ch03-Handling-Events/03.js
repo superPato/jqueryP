a6d64bea22c1a198739523bcb7986ee2a2c9bcde
Cesar <chechaacual@gmail.com>
2018-08-10 17:30:47 -0600

    Refactoring usando Encadenamiento e Iteracion implicita

--- ch03-Handling-Events/03.js
+++ ch03-Handling-Events/03.js
@@ -3,24 +3,21 @@ $(document).ready(function () {
     $('#switcher-default')
         .addClass('selected')
         .on('click', function () {
-            $('body').removeClass('narrow');
-            $('body').removeClass('large');
-            $('#switcher button').removeClass('selected');
-            $(this).addClass('selected');
+            $('body').removeClass('narrow').removeClass('large');
         });
 
     $('#switcher-narrow').on('click', function () {
-        $('body').addClass('narrow');
-        $('body').removeClass('large');
-        $('#switcher button').removeClass('selected');
-        $(this).addClass('selected');
+        $('body').addClass('narrow').removeClass('large');
     })
 
     $('#switcher-large').on('click', function () {
-        $('body').removeClass('narrow');
-        $('body').addClass('large');
+                  // Encadenamiento
+        $('body').removeClass('narrow').addClass('large');
+    });
+
+    $('#switcher button').on('click', function () {
+        // iteracion implicita
         $('#switcher button').removeClass('selected');
         $(this).addClass('selected');
     });
-
 });
