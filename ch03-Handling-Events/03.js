d13732bac9287cfb924b0458cfede3293fbc773e
Cesar <chechaacual@gmail.com>
2018-08-10 18:22:01 -0600

    Refactoring a controlador general de boton

--- ch03-Handling-Events/03.js
+++ ch03-Handling-Events/03.js
@@ -1,22 +1,18 @@
 $(document).ready(function () {
 
-    $('#switcher-default')
-        .addClass('selected')
-        .on('click', function () {
-            $('body').removeClass();
-        });
-
-    $('#switcher-narrow').on('click', function () {
-        $('body').removeClass().addClass('narrow');
-    })
-
-    $('#switcher-large').on('click', function () {
-        // parametro removeClass opcional
-        $('body').removeClass().addClass('large');
-    });
-
+    $('#switcher-default').addClass('selected');
     $('#switcher button').on('click', function () {
+        $('body').removeClass();
         $('#switcher button').removeClass('selected');
         $(this).addClass('selected');
     });
+
+    $('#switcher-narrow').on('click', function () {
+        $('body').addClass('narrow');
+    })
+
+    $('#switcher-large').on('click', function () {
+        $('body').addClass('large');
+    });
+    
 });
