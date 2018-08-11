33d06b48d42ee29fd65dfbc21e8123abc40f039b
Cesar <chechaacual@gmail.com>
2018-08-10 18:15:29 -0600

    Refactoring removeClass opcional

--- ch03-Handling-Events/03.js
+++ ch03-Handling-Events/03.js
@@ -3,20 +3,19 @@ $(document).ready(function () {
     $('#switcher-default')
         .addClass('selected')
         .on('click', function () {
-            $('body').removeClass('narrow').removeClass('large');
+            $('body').removeClass();
         });
 
     $('#switcher-narrow').on('click', function () {
-        $('body').addClass('narrow').removeClass('large');
+        $('body').removeClass().addClass('narrow');
     })
 
     $('#switcher-large').on('click', function () {
-                  // Encadenamiento
-        $('body').removeClass('narrow').addClass('large');
+        // parametro removeClass opcional
+        $('body').removeClass().addClass('large');
     });
 
     $('#switcher button').on('click', function () {
-        // iteracion implicita
         $('#switcher button').removeClass('selected');
         $(this).addClass('selected');
     });
