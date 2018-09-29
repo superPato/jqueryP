734e6163e4f4521b7c2459f337a80e63a3bae716
Cesar Acual <chechaacual@gmail.com>
2018-09-29 15:43:27 -0600

    Usar stopPropagation para dener la progagacion del evento

--- ch03-Handling-Events/03.js
+++ ch03-Handling-Events/03.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
 
     $('#switcher').click(function (event) {
-        if (event.target == this) {
-            $('#switcher button').toggleClass('hidden');
-        }
+        $('#switcher button').toggleClass('hidden');
     });
 
     $('#switcher h3').hover(function () {
@@ -13,11 +11,12 @@ $(document).ready(function () {
     });
 
     $('#switcher-default').addClass('selected');
-    $('#switcher button').click(function () {
+    $('#switcher button').click(function (event) {
         var bodyClass = this.id.split('-')[1];
         $('body').removeClass().addClass(bodyClass);
         $('#switcher button').removeClass('selected');
         $(this).addClass('selected');
+        event.stopProtagation();
     });
     
 });
