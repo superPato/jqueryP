6b83a2f130338dcae9f3859a181f69b5bb1688e7
Cesar Acual <chechaacual@gmail.com>
2018-11-29 21:25:31 -0600

    Increment font div.speech'

---
+++ 4-Styling-and-Animating/04.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	
+	var $speech = $('div.speech');
+
+	$('#switcher-large').click(function () {
+		var num = parseFloat($speech.css('fontSize'));
+		num *= 1.4;
+		$speech.css('fontSize', num + 'px');
+	});
+
+});
