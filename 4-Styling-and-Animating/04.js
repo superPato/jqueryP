aac457742e424412976813f3e4241da6363a0265
Cesar Acual <chechaacual@gmail.com>
2018-12-09 18:25:50 -0600

    Queuing with callbacks

--- 4-Styling-and-Animating/04.js
+++ 4-Styling-and-Animating/04.js
@@ -69,8 +69,11 @@ $(document).ready(function () {
 	$('p').eq(2)
 		.css('border', '1px solid #333')
 		.click(function () {
-								   // pointer next element "p"
-			$(this).slideUp('slow').next().slideDown('slow');
+			var $clickedItem = $(this);
+
+			$clickedItem.next().slideDown('slow', function () {
+				$clickedItem.slideUp('slow');
+			});
 		});
 
 	$('p').eq(3).css('backgroundColor', '#ccc').hide();
